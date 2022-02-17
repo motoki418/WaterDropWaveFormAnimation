5f1b691ebaab109746037b95900f18264fc771d6
motoki418 <snkrs418@gmail.com>
2022-02-17 13:01:07 +0900

    ユーザー画像と水滴画像の表示

--- WaterDropWaveFormAnimation/View/HomeView.swift
+++ WaterDropWaveFormAnimation/View/HomeView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Image("Pic")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+                .padding(10)
+                .background(.white,in: Circle())
+            
+            Text("iJustine")
+                .fontWeight(.semibold)
+                .foregroundColor(.gray)
+                .padding(.bottom, 30)
+            
+            
+            // MARK: Wave Form
+            GeometryReader{ proxy in
+                let size = proxy.size
+                
+                
+                ZStack{
+                    // MARK: Water Drop
+                    Image(systemName: "drop.fill")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.white)
+                    // Streching in X Axis
+                        .scaleEffect(x: 1.1, y: 1)
+                    
+                    // Wave Form Shape
+                    
+                }
+                // 画像を真ん中に
+                .frame(width: size.width, height: size.height, alignment: .center)
+            }
+            .frame(height: 350)
+            
+        }//VStack
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .background(Color("BG"))
     }
 }
 
